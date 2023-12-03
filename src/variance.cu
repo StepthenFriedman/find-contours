37f67d2c6d6bdce8a53f0619d12cb1a77a536df8
StepthenFriedman <143859543+StepthenFriedman@users.noreply.github.com>
2023-12-03 08:07:15 +0800

    12/2

--- src/variance.cu
+++ src/variance.cu
@@ -8,8 +8,6 @@
 //#include <opencv2/core.hpp>
 //#include <opencv2/highgui.hpp>
 
-cudaError_t getVariance(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei);
-
 __global__ void varKernal(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
 {
     unsigned int x = threadIdx.x + blockIdx.x * blockDim.x, y = threadIdx.y + blockIdx.y * blockDim.y, i, j;
@@ -25,7 +23,28 @@ __global__ void varKernal(int *opt, const int *ipt, const unsigned int knl_size,
     }
 }
 
-__global__ void varKernal3D(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
+__global__ void varKernal3D(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei, char flag)
+{
+    unsigned int x = threadIdx.x + blockIdx.x * blockDim.x, y = threadIdx.y + blockIdx.y * blockDim.y, i,j,k;
+    const unsigned int opt_wid=ipt_wid-knl_size+1,opt_hei=ipt_hei-knl_size+1;
+    double sum=0.,temp=0.,avg=0.;
+    if (x<(ipt_wid)&&y<(ipt_hei)) {
+        for (k=0;k<3;k++) {
+            avg=0.;
+            for (i=0;i<knl_size;i++) for (j=0;j<knl_size;j++) avg+=ipt[((y+i)*ipt_wid+(x+j))*3+k];
+            avg/=(double)(knl_size*knl_size);
+            for (i=0;i<knl_size;i++) for (j=0;j<knl_size;j++) temp=avg-ipt[((y+i)*ipt_wid+(x+j))*3+k],sum+=temp*temp;
+            opt[(y*opt_wid+x)*3+k]=sum;
+        }
+        sum/=(double)(knl_size*knl_size)*3;
+        if (flag==1) sum=sqrt(sum)*9;
+        if (sum>255.) sum=255.;
+        opt[(y*opt_wid+x)*3]=opt[(y*opt_wid+x)*3+1]=opt[(y*opt_wid+x)*3+2]=sum;
+    }
+}
+
+/*
+__global__ void conv3D(int *opt, const int *ipt, const int *knl, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
 {
     unsigned int x = threadIdx.x + blockIdx.x * blockDim.x, y = threadIdx.y + blockIdx.y * blockDim.y, i,j,k;
     const unsigned int opt_wid=ipt_wid-knl_size+1,opt_hei=ipt_hei-knl_size+1;
@@ -42,13 +61,14 @@ __global__ void varKernal3D(int *opt, const int *ipt, const unsigned int knl_siz
         opt[(y*opt_wid+x)*3]=opt[(y*opt_wid+x)*3+1]=opt[(y*opt_wid+x)*3+2]=sum;
     }
 }
+*/
 
-__global__ void poolingKernal(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
+__global__ void avgKernal(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
 {
     unsigned int x = threadIdx.x + blockIdx.x * blockDim.x, y = threadIdx.y + blockIdx.y * blockDim.y, i,j,k;
     const unsigned int opt_wid=ipt_wid-knl_size+1,opt_hei=ipt_hei-knl_size+1;
-    double sum=0.,temp=0.,avg=0.;
-    if (x<(opt_wid)&&y<(opt_hei)) for (k=0;k<3;k++) {
+    double avg=0.;
+    if (x<(ipt_wid)&&y<(ipt_hei)) for (k=0;k<3;k++) {
         avg=0.;
         for (i=0;i<knl_size;i++) for (j=0;j<knl_size;j++) avg+=ipt[((y+i)*ipt_wid+(x+j))*3+k];
         avg/=(double)(knl_size*knl_size);
@@ -56,11 +76,218 @@ __global__ void poolingKernal(int *opt, const int *ipt, const unsigned int knl_s
     }
 }
 
+__global__ void poolingKernal(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
+{
+    unsigned int x = threadIdx.x + blockIdx.x * blockDim.x, y = threadIdx.y + blockIdx.y * blockDim.y, i,j,k;
+    const unsigned int opt_wid=ipt_wid-knl_size+1,opt_hei=ipt_hei-knl_size+1;
+    int max;
+    if (x<(ipt_wid)&&y<(ipt_hei)) for (k=0;k<3;k++) {
+        max=0.;
+        for (i=0;i<knl_size;i++) for (j=0;j<knl_size;j++) if (ipt[((y+i)*ipt_wid+(x+j))*3+k]>max) max=ipt[((y+i)*ipt_wid+(x+j))*3+k];
+        opt[(y*opt_wid+x)*3+k]=max;
+    }
+}
+
+
+__global__ void thresholdKernal(int *opt, const int *ipt, const unsigned int ipt_wid, const unsigned int ipt_hei)
+{
+    unsigned int x = threadIdx.x + blockIdx.x * blockDim.x, y = threadIdx.y + blockIdx.y * blockDim.y, i,j,k;
+    //if (x<(ipt_wid)&&y<(ipt_hei)) if ()
+}
+
+cudaError_t getVarPol3D(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei, char flag)
+{
+    int *dev_ipt=NULL,*dev_var=NULL,*dev_opt=NULL;
+    const unsigned int var_wid=ipt_wid-knl_size+1,var_hei=ipt_hei-knl_size+1;
+    const unsigned int opt_wid=var_wid-knl_size+1,opt_hei=var_hei-knl_size+1;
+    cudaError_t cudaStatus;
+    dim3 blocks_var((var_wid+31)/32,(var_hei+31)/32);
+    dim3 blocks_opt((var_wid+31)/32,(var_hei+31)/32);
+    dim3 threadsPerBlock(32,32);
+    //printf("ipt:(%d,%d)->%d opt:(%d,%d)->%d\n",ipt_wid,ipt_hei,ipt_wid*ipt_hei*3,var_wid,var_hei,var_wid*opt_hei*3);
+
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+ 
+    cudaStatus = cudaMalloc((void**)&dev_var, var_wid*var_hei*3 * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_opt, opt_wid*opt_hei*3 * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_ipt, ipt_wid*ipt_hei*3 * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_ipt, ipt, ipt_wid*ipt_hei*3 * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    varKernal3D<<< blocks_var, threadsPerBlock >>>(dev_var, dev_ipt, knl_size, ipt_wid, ipt_hei, flag);
+    poolingKernal<<<blocks_opt, threadsPerBlock >>>(dev_opt, dev_var, knl_size, var_wid, var_hei);
+
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "varKernal launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(opt, dev_opt, opt_wid*opt_hei*3 * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_var);
+    cudaFree(dev_ipt);
+    
+    return cudaStatus;
+}
+
+cudaError_t getVar3D(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei, char flag)
+{
+    int *dev_ipt=NULL,*dev_var=NULL;
+    const unsigned int var_wid=ipt_wid-knl_size+1,var_hei=ipt_hei-knl_size+1;
+    cudaError_t cudaStatus;
+    dim3 blocks_var((var_wid+31)/32,(var_hei+31)/32);
+    dim3 threadsPerBlock(32,32);
+    //printf("ipt:(%d,%d)->%d opt:(%d,%d)->%d\n",ipt_wid,ipt_hei,ipt_wid*ipt_hei*3,var_wid,var_hei,var_wid*opt_hei*3);
+
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+ 
+    cudaStatus = cudaMalloc((void**)&dev_var, var_wid*var_hei*3 * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_ipt, ipt_wid*ipt_hei*3 * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_ipt, ipt, ipt_wid*ipt_hei*3 * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    varKernal3D<<< blocks_var, threadsPerBlock >>>(dev_var, dev_ipt, knl_size, ipt_wid, ipt_hei, flag);
+
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "varKernal launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(opt, dev_var, var_wid*var_hei*3 * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_var);
+    cudaFree(dev_ipt);
+    
+    return cudaStatus;
+}
+
+cudaError_t getAvg3D(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
+{
+    int *dev_ipt=NULL,*dev_opt=NULL;
+    const unsigned int opt_wid=ipt_wid-knl_size+1,opt_hei=ipt_hei-knl_size+1;
+    cudaError_t cudaStatus;
+    dim3 blocks((opt_wid+31)/32,(opt_hei+31)/32);
+    dim3 threadsPerBlock(32,32);
+    printf("ipt:(%d,%d)->%d opt:(%d,%d)->%d\n",ipt_wid,ipt_hei,ipt_wid*ipt_hei*3,opt_wid,opt_hei,opt_wid*opt_hei*3);
+
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+ 
+    cudaStatus = cudaMalloc((void**)&dev_opt, opt_wid*opt_hei*3 * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_ipt, ipt_wid*ipt_hei*3 * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_ipt, ipt, ipt_wid*ipt_hei*3 * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    avgKernal<<< blocks, threadsPerBlock >>>(dev_opt, dev_ipt, knl_size, ipt_wid, ipt_hei);
+
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "varKernal launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(opt, dev_opt, opt_wid*opt_hei*3 * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_opt);
+    cudaFree(dev_ipt);
+    
+    return cudaStatus;
+}
 int dbg2(){
     int i,j,k;
     std::vector<unsigned char> in_image;
-    unsigned int ipt_wid,ipt_hei,knl_size=3;
-    char error=lodepng::decode(in_image, ipt_wid, ipt_hei, "../resource/Lenna.png",LCT_RGBA);
+    unsigned int ipt_wid,ipt_hei,knl_size=5;
+    char error=lodepng::decode(in_image, ipt_wid, ipt_hei, "../resource/input/Valve_org.png",LCT_RGBA);
     const unsigned int opt_wid=ipt_wid-knl_size+1,opt_hei=ipt_hei-knl_size+1;
 
 
@@ -72,7 +299,7 @@ int dbg2(){
     }
     printf("%s\n",lodepng_error_text(error));
 
-    cudaError_t cudaStatus = getVariance(output_image,input_image,knl_size,ipt_wid,ipt_hei);
+    cudaError_t cudaStatus = getVar3D(output_image,input_image,knl_size,ipt_wid,ipt_hei,1);
     std::vector<unsigned char> out_image;
     
     for (i=0;i<opt_hei;i++) for (j=0;j<opt_wid;j++) {
@@ -82,13 +309,44 @@ int dbg2(){
         out_image.push_back(255);
     }
     printf("ok!\n");
-    error= lodepng::encode("../resource/result.png", out_image, opt_wid, opt_hei);
+    error= lodepng::encode("../resource/output/result.png", out_image, opt_wid, opt_hei);
     printf("%s\n",lodepng_error_text(error));
     return 0;
 }
-//rgb
-int dbg3(){
+
+int dbg4(){
     int i,j,k;
+    std::vector<unsigned char> in_image;
+    unsigned int ipt_wid,ipt_hei,knl_size=3;
+    char error=lodepng::decode(in_image, ipt_wid, ipt_hei, "../resource/input/Valve_original.png",LCT_RGBA);
+    const unsigned int opt_wid=ipt_wid-knl_size*2+1,opt_hei=ipt_hei-knl_size*2+1;
+
+
+    printf("size:%lu wid:%u hei:%u\n",in_image.size(),ipt_wid,ipt_hei);
+    int* input_image = new int[ipt_wid*ipt_hei*3];
+    int* output_image = new int[ipt_wid*ipt_hei*3];
+    for (i=0;i<ipt_hei;i++) for (j=0;j<ipt_wid;j++) for (k=0;k<3;k++){
+        input_image[(i*ipt_wid+j)*3+k]=in_image[(i*ipt_wid+j)*4+k];
+    }
+    printf("%s\n",lodepng_error_text(error));
+
+    cudaError_t cudaStatus = getAvg3D(output_image,input_image,knl_size,ipt_wid,ipt_hei);
+    std::vector<unsigned char> out_image;
+    
+    for (i=0;i<opt_hei;i++) for (j=0;j<opt_wid;j++) {
+        for (k=0;k<3;k++){
+            out_image.push_back(output_image[(i*opt_wid+j)*3+k]);
+        }
+        out_image.push_back(255);
+    }
+    printf("ok!\n");
+    error= lodepng::encode("../resource/output/result.png", out_image, opt_wid, opt_hei);
+    printf("%s\n",lodepng_error_text(error));
+    return 0;
+}
+
+int dbg3(){
+    int i,j;
     const unsigned int opt_wid=512,opt_hei=512;
 
     std::vector<unsigned char> out_image;
@@ -122,7 +380,7 @@ int dbg1(){
                                     21,22,23,24,25};
     int opt[ipt_hei-knl_size+1][ipt_wid-knl_size+1]={0};
 
-    cudaError_t cudaStatus = getVariance( &opt[0][0], &ipt[0][0], knl_size, ipt_wid, ipt_hei);
+    cudaError_t cudaStatus = getVar3D( &opt[0][0], &ipt[0][0], knl_size, ipt_wid, ipt_hei,0);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "addWithCuda failed!");
         return 1;
@@ -141,64 +399,4 @@ int main(){
         return 1;
     }
     return 0;
-}
-
-cudaError_t getVariance(int *opt, const int *ipt, const unsigned int knl_size, const unsigned int ipt_wid, const unsigned int ipt_hei)
-{
-    int *dev_ipt=NULL,*dev_opt=NULL;
-    const unsigned int opt_wid=ipt_wid-knl_size+1,opt_hei=ipt_hei-knl_size+1;
-    cudaError_t cudaStatus;
-    dim3 blocks((opt_wid+31)/32,(opt_hei+31)/32);
-    dim3 threadsPerBlock(32,32);
-    printf("ipt:(%d,%d)->%d opt:(%d,%d)->%d\n",ipt_wid,ipt_hei,ipt_wid*ipt_hei*3,opt_wid,opt_hei,opt_wid*opt_hei*3);
-
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
- 
-    cudaStatus = cudaMalloc((void**)&dev_opt, opt_wid*opt_hei*3 * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_ipt, ipt_wid*ipt_hei*3 * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_ipt, ipt, ipt_wid*ipt_hei*3 * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    varKernal3D<<< blocks, threadsPerBlock >>>(dev_opt, dev_ipt, knl_size, ipt_wid, ipt_hei);
-
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "varKernal launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(opt, dev_opt, opt_wid*opt_hei*3 * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_opt);
-    cudaFree(dev_ipt);
-    
-    return cudaStatus;
 }
